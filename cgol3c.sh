cf23bca7bb1cacc109cf3867d82bcf0ea9c44faa
TobiasAAU <2biasbl@gmail.com>
2023-04-30 13:05:48 +0200

    things work

--- cgol3c.sh
+++ cgol3c.sh
@@ -1,3 +1,7 @@
 
 
-clear ; java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc CGOL3experiment.jj ; javac JavaParser.java ; java JavaParser DomCard.txt
+java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc CGOL3.jj
+javac JavaParser.java
+java JavaParser inputfile.cgol3
+
+#clear ; java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc CGOL3experiment.jj ; javac JavaParser.java ; java JavaParser DomCard.txt
