3fcca610d7e9d94db80d3bfef647cc0cb32591b6
TobiasAAU <2biasbl@gmail.com>
2023-04-30 13:22:40 +0200

    remove generated files

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc CGOL3.jj
+#javac JavaParser.java
+#java JavaParser inputfile.cgol3
+
+rm *.class *.java
+
+#clear ; java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc CGOL3experiment.jj ; javac JavaParser.java ; java JavaParser DomCard.txt
