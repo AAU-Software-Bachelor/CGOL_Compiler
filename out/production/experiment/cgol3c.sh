e0f926fb0724eaba7c7258ea1aa95296fb785364
ThomasNyk <nykjaerthomas+github@gmail.com>
2023-05-01 13:26:55 +0200

    Fixed shit

--- out/production/experiment/cgol3c.sh
+++
@@ -1,8 +0,0 @@
-clear
-
-java -jar ~/code/jtb.jar CGOL3.jj
-java -cp /home/blueorc/Desktop/Programs/javacc/target/javacc.jar javacc jtb.out.jj
-
-javac JavaParser.java
-
-java JavaParser inputfile.cgol3
